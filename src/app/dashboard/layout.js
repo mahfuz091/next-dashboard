e32e4c4bf5ce4899445803121777a90205822086
mahfuz091 <engmahfuz14@gmail.com>
2024-03-18 23:15:24 +0600

    sidebar toggol update

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,18 +2,27 @@
 import React, { useState } from "react";
 import Sidebar from "../ui/dashboard/Sidebar/Sidebar";
 import styles from "../ui/dashboard/dashboard.module.css";
+import Navbar from "../ui/dashboard/Navbar/Navbar";
 
 const DashboardLayout = ({ children }) => {
-  const { sidebarOpen, setSidebarOpen } = useState(false);
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+  console.log(openSidebarToggle);
   return (
     <div className={styles.container}>
-      <div className={styles.menu}>
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div
+        className={
+          openSidebarToggle ? `${styles.menu} ${styles.menuOpen}` : styles.menu
+        }
+      >
+        <Sidebar />
       </div>
       <div className={styles.content}>
-        {/* <Navbar/> */}
+        <Navbar OpenSidebar={OpenSidebar} />
         {children}
-        {/* <Footer/> */}
+        {/* <Footer /> */}
       </div>
     </div>
   );
