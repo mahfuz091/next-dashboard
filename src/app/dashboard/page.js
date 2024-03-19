07fd048f0248c6cb5b0348417fe9b5d20cd05b51
mahfuz091 <engmahfuz14@gmail.com>
2024-03-19 17:29:47 +0600

    added card

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "../ui/dashboard/dashboard.module.css";
 import Chart from "../ui/dashboard/Chart/Chart";
+import { cards } from "../lib/data";
+import Card from "./Card/Card";
 
 const DashboardPage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <div className={styles.cards}>
-          {/* {cards.map((item) => (
+          {cards.map((item) => (
             <Card item={item} key={item.id} />
-          ))} */}
+          ))}
         </div>
         {/* <Transactions /> */}
         <Chart />
